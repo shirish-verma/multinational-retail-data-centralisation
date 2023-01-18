474c78bc996d9ff61690627a5952e2943296ce75
shirish-verma <shirishverma@gmail.com>
2023-01-18 12:38:37 +0000

    More pandas practicals.

--- practicals_pandas.py
+++ practicals_pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import string
 from yaml import safe_load
 
@@ -40,4 +41,27 @@ print(f'The mean of base salary of the people in fire department is {fire_basepa
 
 unique_job_titles = sf_sal['JobTitle'].unique()
 print(f'There are {len(unique_job_titles)} unique job titles in the salaries.csv database.')
+
+mask_johns = sf_sal['EmployeeName'].str.contains('JOHN ')
+sf_sal_johns = sf_sal[mask_johns]
+print(len(sf_sal_johns['EmployeeName']))
+
+mask_first_names = sf_sal['EmployeeName'].apply(lambda x : x.split()[-1] if len(x.split()[0]) > 6 else None)
+print(sf_sal[mask_first_names.notnull()])
+# print(sf_sal.head())
+# sf_sal_first_names = sf_sal[mask_first_names]
+# print(sf_sal_first_names)
+
+
+
+# print(np.nan)
+# print(type(np.nan))
+
+# later_date_ISO_format = "2021-02-15"
+# earlier_date_ISO_format = "2021-02-16"
+
+# print(later_date_ISO_format < earlier_date_ISO_format)
+# print(later_date_ISO_format > earlier_date_ISO_format)
+# print(type(later_date_ISO_format))
+
 
